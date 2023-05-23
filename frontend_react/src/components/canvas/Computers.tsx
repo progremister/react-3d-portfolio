234e8d0aa9138e0215930e1a5588f7aa1bc7b646
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-05-23 23:02:44 +0200

    Moved the react part into the frontend_react folder

---
+++ frontend_react/src/components/canvas/Computers.tsx
@@ -0,0 +1,79 @@
+import React, { Suspense, useState, useEffect, lazy} from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+type ComputersProps = { isMobile: boolean }
+
+const Computers = ( {isMobile}: ComputersProps) => {
+  const computer = useGLTF("./macbook_m1/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.35} groundColor='black'/>
+      <directionalLight castShadow position={[-20, -25, -25]} shadow-mapSize={[1024, 1024]} intensity={0.25}>
+        <orthographicCamera attach="shadow-camera" args={[-10, 10, 10, -10]} />
+      </directionalLight>      
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={0.65}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 17 : 17.75}
+        position={isMobile ? [-0.5, -4, -0.75] : [2, -3.2, -2]}
+        rotation={[0, 0.75, 0.0]}
+      />
+    </mesh>
+  );
+};
+
+const LazyComputers = lazy(() => Promise.resolve({ default: Computers }));
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [15, 3, 5], fov: 35 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          // maxPolarAngle={Math.PI / 2}
+          // minPolarAngle={Math.PI / 2.5}
+        />
+        <LazyComputers isMobile={isMobile}/>
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
