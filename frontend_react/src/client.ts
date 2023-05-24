1243b94b73dbde4fcd0caf700d6beb3986fe8a2c
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-05-24 23:09:22 +0200

    Changed the client settings

--- frontend_react/src/client.ts
+++ frontend_react/src/client.ts
@@ -1,14 +1,16 @@
-import sanityClient from '@sanity/client';
+import {createClient, ClientConfig, SanityClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+const clientConfig: ClientConfig = {
+    projectId: 'xhj8ghog'!,
     dataset: 'production',
     apiVersion: '2023-05-23',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN
-});
+    token: 'skpqohUR6z7d7jipT1lsWUvl7oT897lCUiVv1h77ycShAI5MgcUKWObkEFo90xn4huXsfoK19EjkQbAuGpVplqaYPEPgJIC0TSEsM0d1joJ1cZFfGpJpvM6XLlzFEoNzQdmN3C1GXbcXuhhdmJmxuk5qg26HzE0aABQp3QYiZZvTXPliVgBE'!
+};
+
+export const client: SanityClient = createClient(clientConfig);
 
 const builder = imageUrlBuilder(client);
 
-export const urlFor = (source: string) => builder.image(source);
+export const urlFor: any = (source: string) => builder.image(source);
