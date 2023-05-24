cef34ddd5799e4b9d94db18e612a634d9c71cd61
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-05-24 23:03:49 +0200

    Modified some schemas

--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -1,7 +1,7 @@
-import about from './about';
+import about from './abouts';
 import experience from './experience';
 import skills from './skills';
-import works from './works';
+import works from './projects';
 import testimonials from './testimonials';
 import tags from './tags';
 
