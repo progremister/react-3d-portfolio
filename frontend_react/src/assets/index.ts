1fad63a7e48118357e38247d201f269108d5ee27
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-05-29 22:58:56 +0200

    Added assets and constants

---
+++ frontend_react/src/assets/index.ts
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import github from "./github.png";
+import eye from "./eye.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  logo,
+  eye,
+  github,
+  menu,
+  close
+};
